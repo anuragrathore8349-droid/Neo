4db1ce7d3fb54e937877bf9e5e28aa55054cce2a
anuragrathore8349-droid <anuragrathore8349@gmail.com>
2026-06-20 11:35:00 +0530

    Simplify PortfolioHandler by removing unused methods
    Refactor PortfolioHandler to remove unused methods and manage user connections more effectively.

--- server/src/websockets/handlers/portfolio.handler.js
+++ server/src/websockets/handlers/portfolio.handler.js
@@ -1,101 +1,38 @@
-const { Server } = require('socket.io');
 const { logger } = require('../../api/middlewares/logger.middleware');
 
 class PortfolioHandler {
   constructor(io) {
     this.io = io;
-    this.rooms = new Map();
     this.setupEventHandlers();
   }
 
   setupEventHandlers() {
     this.io.of('/portfolio').on('connection', (socket) => {
-      logger.info(`Client connected to portfolio websocket: ${socket.id}`);
+      const userId = socket.user?.userId?.toString();
+      if (!userId) return socket.disconnect(true);
 
-      socket.on('subscribe', (portfolioId) => {
-        this.handleSubscribe(socket, portfolioId);
-      });
+      logger.info(`Portfolio WS connected: user=${userId}`);
 
-      socket.on('unsubscribe', (portfolioId) => {
-        this.handleUnsubscribe(socket, portfolioId);
-      });
+      // Join the user's personal room
+      socket.join(`user:${userId}`);
 
       socket.on('disconnect', () => {
-        this.handleDisconnect(socket);
+        logger.info(`Portfolio WS disconnected: user=${userId}`);
       });
     });
   }
 
-  handleSubscribe(socket, portfolioId) {
-    const room = `portfolio:${portfolioId}`;
-    socket.join(room);
-    
-    if (!this.rooms.has(socket.id)) {
-      this.rooms.set(socket.id, new Set());
-    }
-    this.rooms.get(socket.id).add(room);
-
-    logger.info(`Client ${socket.id} subscribed to portfolio ${portfolioId}`);
+  // Called by trading.service.js and portfolio-history.job.js after updates
+  broadcastPortfolioUpdate(userId, data) {
+    this.io.of('/portfolio').to(`user:${userId}`).emit('portfolio_update', data);
+    logger.debug(`Portfolio update broadcast to user:${userId}`);
   }
 
-  handleUnsubscribe(socket, portfolioId) {
-    const room = `portfolio:${portfolioId}`;
-    socket.leave(room);
-    
-    if (this.rooms.has(socket.id)) {
-      this.rooms.get(socket.id).delete(room);
-    }
-
-    logger.info(`Client ${socket.id} unsubscribed from portfolio ${portfolioId}`);
-  }
-
-  handleDisconnect(socket) {
-    if (this.rooms.has(socket.id)) {
-      this.rooms.delete(socket.id);
-    }
-    logger.info(`Client disconnected from portfolio websocket: ${socket.id}`);
-  }
-
-  // Broadcast portfolio updates
-  broadcastPortfolioUpdate(portfolioId, data) {
-    const room = `portfolio:${portfolioId}`;
-    this.io.of('/portfolio').to(room).emit('portfolioUpdate', {
-      portfolioId,
-      data,
-      timestamp: Date.now()
-    });
-  }
-
-  // Broadcast asset updates
-  broadcastAssetUpdate(portfolioId, assetId, data) {
-    const room = `portfolio:${portfolioId}`;
-    this.io.of('/portfolio').to(room).emit('assetUpdate', {
-      portfolioId,
-      assetId,
-      data,
-      timestamp: Date.now()
-    });
-  }
-
-  // Broadcast performance metrics
-  broadcastPerformanceUpdate(portfolioId, metrics) {
-    const room = `portfolio:${portfolioId}`;
-    this.io.of('/portfolio').to(room).emit('performanceUpdate', {
-      portfolioId,
-      metrics,
-      timestamp: Date.now()
-    });
-  }
-
-  // Broadcast allocation updates
-  broadcastAllocationUpdate(portfolioId, allocation) {
-    const room = `portfolio:${portfolioId}`;
-    this.io.of('/portfolio').to(room).emit('allocationUpdate', {
-      portfolioId,
-      allocation,
-      timestamp: Date.now()
-    });
+  broadcastTransactionUpdate(userId, transaction) {
+    this.io.of('/portfolio').to(`user:${userId}`).emit('transaction_update', transaction);
   }
 }
+
+module.exports = PortfolioHandler;
 
 module.exports = PortfolioHandler;
