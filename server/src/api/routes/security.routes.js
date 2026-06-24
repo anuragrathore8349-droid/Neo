09c75c691be0a28ad44805ce6253f53955bfc178
anuragrathore8349-droid <anuragrathore8349@gmail.com>
2026-06-24 09:37:49 +0530

    Update security.routes.js

--- server/src/api/routes/security.routes.js
+++ server/src/api/routes/security.routes.js
@@ -1,16 +1,20 @@
-const express             = require('express');
-const { authMiddleware }  = require('../middlewares/auth.middleware');
-const securityController  = require('../controllers/security.controller');
+// server/src/api/routes/security.routes.js
+'use strict';
+
+const express            = require('express');
+const { authMiddleware } = require('../middlewares/auth.middleware');
+const securityController = require('../controllers/security.controller');
 
 const router = express.Router();
 router.use(authMiddleware);
 
-router.get('/status',         securityController.getSecurityStatus);
-router.get('/devices',        securityController.getDevices);
-router.delete('/devices/:id', securityController.removeDevice);
-router.get('/activity',       securityController.getActivityLog);    // ← ADD
-router.post('/2fa/setup',     securityController.setup2FA);
-router.post('/2fa/verify',    securityController.verify2FA);
-router.post('/2fa/disable',   securityController.disable2FA);
+router.get('/status',              securityController.getSecurityStatus);
+router.get('/devices',             securityController.getDevices);
+router.delete('/devices/:id',      securityController.removeDevice);
+router.get('/activity',            securityController.getActivityLog);
+router.post('/2fa/setup',          securityController.setup2FA);
+router.post('/2fa/verify',         securityController.verify2FA);
+router.post('/2fa/disable',        securityController.disable2FA);
+router.put('/permissions/:id',     securityController.togglePermission);   // NEW
 
 module.exports = router;
